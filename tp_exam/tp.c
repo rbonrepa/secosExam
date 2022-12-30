1589eb63d0def752241f5e9bd8cf531ff1f5ca77
rbonrepa <remi.bonrepaux09@gmail.com>
2022-12-30 09:54:22 +0100

    interruption qui marche

--- tp_exam/tp.c
+++ tp_exam/tp.c
@@ -17,7 +17,7 @@ __attribute__((section(".sys_count"))) void sys_counter(uint32_t*counter){
 
 /* Task 1: counteur in shared memory. */
 __attribute__((section(".user1"))) void increment_counter(){
-    uint32_t *shared_mem = (uint32_t *)SHARED_MEMORY;
+    uint32_t *shared_mem = (uint32_t *)0x800000;
     *shared_mem = 0;
     while (1)
       (*shared_mem)++;
@@ -48,44 +48,77 @@ void set_mapping(pde32_t *pgd, pte32_t *first_ptb, unsigned int flags){
         // Add the ptb to the pgd
         pg_set_entry(&pgd[ptb_num], flags, page_nr(ptb));
     }
-    // Enable paging 
-    set_cr3(pgd);   
+       
+}
+
+void init_seg_desc(uint64_t limit, uint64_t base, uint64_t type, uint64_t dpl){
+    int i;
+    gdt_reg_t gdtr;
+    get_gdtr(gdtr);
+    if (gdtr.limit == 0){
+        i = 0;
+    }
+    else{
+        i = (gdtr.limit + 1)/sizeof(seg_desc_t);
+    }
+    gdt[i].raw = 0ULL;
+    gdt[i].base_1 = base;
+    gdt[i].base_2 = base >> 16;
+    gdt[i].base_3 = base >> 24;
+    gdt[i].limit_1 = limit;
+    gdt[i].limit_2 = limit >> 16;
+    gdt[i].type = type;
+    gdt[i].dpl = dpl;
+
+    gdt[i].s = 1; 
+    gdt[i].p = 1;
+    gdt[i].avl = 0;
+    gdt[i].l = 0;
+    gdt[i].d = 1;
+    gdt[i].g = 1;
+
+    if (i == 0){
+        gdtr.limit = sizeof(seg_desc_t) - 1;
+        gdt[0].raw = 0ULL;
+    }
+    else{
+        gdtr.limit = gdtr.limit + sizeof(seg_desc_t);
+    }
+    set_gdtr(gdtr);
 }
 
 //--- Define kernel ---//
 /* This function allows to initialise gdt, tss, registers, and pagination. */
 void set_kernel(){
+    // Activation of pagination for kenerl: set pgd at 0x310000, ptb at 0x311000
+    set_mapping((pde32_t *)KERNEL_PGD, (pte32_t *)KERNEL_PTB, PG_KRN | PG_RW);
+
+    // Enable paging 
+    set_cr3(KERNEL_PGD);
+
     // Initialisaion gdt
-    gdt_reg_t gdtr = {
-	desc : gdt,
-	limit : sizeof(gdt) - 1
-    };
-	
-    // Initialisation tss
-    memset(&tss, 0, sizeof tss); // reserve memory
-    //tss_dsc(&gdt[TSS], (offset_t)&tss); // record in the gdt
+    gdt_reg_t gdtr;
+    gdtr.limit = 0;
+    gdtr.desc = gdt;
     set_gdtr(gdtr);
+    init_seg_desc(0,0,0,0); //1er segment null Index0
+    init_seg_desc(0xfffff,0,SEG_DESC_CODE_XR,0); //Segment code Ring 0 Index1
+    init_seg_desc(0xfffff,0,SEG_DESC_DATA_RW,0); //Segment data Ring 0 Index2
+    init_seg_desc(0xfffff,0,SEG_DESC_CODE_XR,3); //Segment Code Ring 3 Index3
+    init_seg_desc(0xfffff,0,SEG_DESC_DATA_RW,3); //Segment data Ring 3 index4
 	
     // Initialisation registres in gdt
-    /* En commentaire car fait bugguer mais important
-    set_cs(gdt_krn_seg_sel(RING0_CODE)); 
+    set_cs(gdt_krn_seg_sel(RING0_CODE));
     set_ss(gdt_krn_seg_sel(RING0_DATA));
     set_ds(gdt_krn_seg_sel(RING0_DATA));
     set_es(gdt_krn_seg_sel(RING0_DATA));
     set_fs(gdt_krn_seg_sel(RING0_DATA));
-    set_gs(gdt_krn_seg_sel(RING0_DATA));*/
-	
-    // Activation of pagination for kenerl: set pgd at 0x310000, ptb at 0x311000
-    set_mapping((pde32_t *)KERNEL_PGD, (pte32_t *)KERNEL_PTB, PG_KRN | PG_RW);
+    set_gs(gdt_krn_seg_sel(RING0_DATA));
 }
 
 //--- Define tasks ---//
 /* This function allows to create one task with pgd/ptb, kernel stack and add the task to the shared memory. */
 void set_task(uint32_t user_task){
-    // Create pgd/ptb
-    pde32_t *pgd_task = (pde32_t *)(user_task + USER_PGD_OFFSET);
-    pte32_t *ptb_task = (pte32_t *)(user_task + USER_PTB_OFFSET);
-    set_mapping(pgd_task, ptb_task, PG_USR | PG_RW);
 
     // Set the kernel stack
     uint32_t *user_kernel_esp = (uint32_t *)(user_task + USER_KERNEL_STACK_START_OFFSET);
@@ -98,18 +131,25 @@ void set_task(uint32_t user_task){
     print_stack_tasks(user_kernel_esp, number_tasks);
 
     // Create the shared memory
-    pte32_t *ptb_shared = (pte32_t *)(user_task + USER_PTB_OFFSET + 2 * 4096);
-    pg_set_entry(&pgd_task[2], PG_USR | PG_RW, page_nr(ptb_shared)); // pgd[0] = ptb1, pgd[1] = ptb2, pgd[2] = ptb_shared
-    pg_set_entry(&ptb_shared[0], PG_USR | PG_RW, page_nr(SHARED_MEMORY)); // Pointe vers une même adresse
-
+    //pte32_t *ptb_shared = (pte32_t *)(user_task + USER_PTB_OFFSET + 2 * 4096);
+    //pg_set_entry(&pgd_task[2], PG_USR | PG_RW, page_nr(ptb_shared)); // pgd[0] = ptb1, pgd[1] = ptb2, pgd[2] = ptb_shared
+    //pg_set_entry(&ptb_shared[0], PG_USR | PG_RW, page_nr(SHARED_MEMORY)); // Pointe vers une même adresse
+    
+    tasks[number_tasks] = user_task;
     number_tasks++;
+
+    // Create pgd/ptb
+    pde32_t *pgd_task = (pde32_t *)(user_task + USER_PGD_OFFSET);
+    pte32_t *ptb_task = (pte32_t *)(user_task + USER_PTB_OFFSET);
+    set_mapping(pgd_task, ptb_task, PG_USR | PG_RW);
 }
 
 //--- Kernel interruptions ---//
 /* This function allows to change task (task1->task2 and reverse). */
 void handler_scheduler(){
-    debug("Scheduler");
-    // They are no task 
+    debug("Counteur Interruption (32) !\n");
+    asm volatile("int $0x80;\n");
+    /* They are no task 
     if (number_tasks <= 0)
         return;
 
@@ -131,20 +171,21 @@ void handler_scheduler(){
         tss.s0.esp = user_kernel_esp;
         //Change context
 
-        /* Changer de contexte
+         Changer de contexte
         //set_cr3(task + USER_PGD_OFFSET);
         int32_t esp = user_kernel_esp - (4 * 13); // 13 entries were pushed to the stack
         asm volatile(
         "mov %0, %%eax\n"
         :
-        : "r"(esp));*/
-    }
+        : "r"(esp));
+    }*/
 }
 
 /* This function allows to print the task2 with a syscall. */
 void handle_kernel_print(){
     // Code to trint the counteur with kernel syscall
-    debug("Print counteur");
+    debug("Syscall interruption (80) !\n");
+    //asm volatile("int $0x32;\n");
 }
 
 //--- Start tasks ---//
@@ -175,7 +216,7 @@ void start_tasks(){
     idt[0x80].dpl = SEG_SEL_USR; 
 
     // Start
-    //asm volatile("int $0x80;\n");  
+    asm volatile("int $32;\n");  
 }
 
 void tp(){
@@ -184,9 +225,8 @@ void tp(){
 
     // Set up kernel: init gdt, tss, register and pagination
     set_kernel();
-    
+    set_cr0(get_cr0() | CR0_PG | CR0_PE);
     // Set up 2 tasks: init pagination, kernel stack and shared memory
-    //set_cr0(get_cr0() | CR0_PG | CR0_PE);
     set_task((uint32_t)increment_counter);
     set_task((uint32_t)print_counter);
     	
